68d77f9fb2deeac811766e8f761754fb207328ad
dichopter <43945391+dichopter@users.noreply.github.com>
2018-10-08 00:58:53 -0400

    Update index.js
    Changing image links to be relative

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ function addImages (numImages) {
       a.setAttribute("imageNum", numImages);
       col.appendChild(a);
       var img = document.createElement("img");
-      img.src = "http://dichopter.epizy.com/stereogram/images/image"+numImages+".jpg";
+      img.src = "/stereogram/images/image"+numImages+".jpg";
       //http://dichopter.epizy.com/stereogram/images/image9.jpg
       a.appendChild(img);  
       row.appendChild(col);

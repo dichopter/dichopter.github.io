29304ea247f3bd08d86b62409ea06f00c6d296d9
dichopter <dichopter@gmail.com>
2018-10-25 13:00:06 -0400

    Testing a switch back to the old fetch method

--- sworkerFull.js
+++ sworkerFull.js
@@ -22,51 +22,51 @@ self.addEventListener('activate', function (event) {
   );
 });
 
-// self.addEventListener('fetch', function (event) {
-//   console.log("Service worker fetching...");
-//   event.respondWith(
-//     fetch(event.request)
-//     .then(function (res) {
-//       // Make a copy/clone of whatever is from the server
-//       var resClone = res.clone(); // built-in method to clone
-//       // Open a new cache
-//       caches.open(staticCacheName)
-//         .then(function (cache) {
-//           // Add the currently online server response to the cache
-//           cache.put(event.request, resClone);
-//         });
-//       return res;
-//     }).catch(function (error) { // If the connection is droppped, then load from here
-//       caches.match(event.request)
-//         .then(function (res) {
-//           return res;
-//         });
-//     })
-//   );
-// });
-
-
-
 self.addEventListener('fetch', function (event) {
   console.log("Service worker fetching...");
   event.respondWith(
-    caches.match(event.request).catch(function () {
-      return fetch(event.request);
-    }).then(function (res) {
+    fetch(event.request)
+    .then(function (res) {
       // Make a copy/clone of whatever is from the server
-      var response = res.clone();
+      var resClone = res.clone(); // built-in method to clone
       // Open a new cache
       caches.open(staticCacheName)
         .then(function (cache) {
           // Add the currently online server response to the cache
-          cache.put(event.request, response);
+          cache.put(event.request, resClone);
         });
-      return response;
+      return res;
     }).catch(function (error) { // If the connection is droppped, then load from here
       caches.match(event.request)
         .then(function (res) {
           return res;
         });
     })
-    );
+  );
 });
+
+
+
+// self.addEventListener('fetch', function (event) {
+//   console.log("Service worker fetching...");
+//   event.respondWith(
+//     caches.match(event.request).catch(function () {
+//       return fetch(event.request);
+//     }).then(function (res) {
+//       // Make a copy/clone of whatever is from the server
+//       var response = res.clone();
+//       // Open a new cache
+//       caches.open(staticCacheName)
+//         .then(function (cache) {
+//           // Add the currently online server response to the cache
+//           cache.put(event.request, response);
+//         });
+//       return response;
+//     }).catch(function (error) { // If the connection is droppped, then load from here
+//       caches.match(event.request)
+//         .then(function (res) {
+//           return res;
+//         });
+//     })
+//     );
+// });

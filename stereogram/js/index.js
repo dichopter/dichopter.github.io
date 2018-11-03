caeaaabaebef713e9ee2a87b989666342bc46473
dichopter <dichopter@gmail.com>
2018-11-03 12:51:17 -0400

    trying to fix rotation resize event

--- stereogram/js/index.js
+++ stereogram/js/index.js
@@ -99,11 +99,12 @@ function draw() {
 
 function windowResized() {
   slider.elt.value = calculateOptimum(.1, 5);
-  resizeCanvas(window.innerWidth, window.innerHeight);
+  resizeCanvas(windowWidth, windowHeight);
   switchButton.style("top", (window.height-100)+"px");
   switchButton.style("left", "0px");
   resetButton.style("top", (window.height-100)+"px");
   resetButton.style("left", (window.width-100)+"px");
+  
 }
 
 function calculateOptimum(min, max) {

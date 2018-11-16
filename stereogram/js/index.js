f87a43aeda16bf70f3d8f6a029314bbe968c12f6
dichopter <dichopter@gmail.com>
2018-11-16 13:08:36 -0500

    made the images literally clear but working on a make visible method that is triggered by mouse movement touch or button press

--- stereogram/js/index.js
+++ stereogram/js/index.js
@@ -61,12 +61,12 @@ function setup() {
   slider.style('width', '10px');
 
   switchButton = createButton('');
-  styleElement(switchButton, ["padding", "0", "background-color", "transparent", /*"background-image", "url('https://raw.githubusercontent.com/dichopter/dichopter.github.io/master/stereogram/js/switch.png')",*/ "background-size", "cover", "width", "100px", "height", "100px", "border", "none"]);
+  styleElement(switchButton, ["padding", "0", "background-color", "transparent", "background-image", "url('https://raw.githubusercontent.com/dichopter/dichopter.github.io/master/stereogram/js/switch.png')", "background-size", "cover", "width", "100px", "height", "100px", "border", "none", "opacity", "0"]);
   switchButton.position(0, window.innerHeight-100);
   switchButton.mousePressed(switchImages);
   
   resetButton = createButton(''); 
-  styleElement(resetButton, ["padding", "0", "background-color", "transparent", /*"background-image", "url('https://raw.githubusercontent.com/dichopter/dichopter.github.io/master/stereogram/js/reset.png')", */ "background-size", "cover", "width", "100px", "height", "100px", "border", "none"]);
+  styleElement(resetButton, ["padding", "0", "background-color", "transparent", "background-image", "url('https://raw.githubusercontent.com/dichopter/dichopter.github.io/master/stereogram/js/reset.png')", "background-size", "cover", "width", "100px", "height", "100px", "border", "none", "opacity", "0"]);
   resetButton.position(window.innerWidth-100, window.innerHeight-100);
   resetButton.mousePressed(resetImages);
   
@@ -97,6 +97,12 @@ function styleElement(element, styles) {
     element.style(styles[i], styles[i+1]);
   }
 }
+
+
+function showButtons() {
+
+}
+
 
 function calculateOptimum(min, max) {
   var optimum = 0.05;

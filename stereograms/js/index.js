e07b0528c2ad0ac9fa3452b2946b58ec88c0ffd9
dichopter <dichopter@gmail.com>
2018-11-18 10:47:31 -0500

    updated image resolutions and positions removed extra images

--- stereograms/js/index.js
+++ stereograms/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   //sessionStorage.imageNum = "1";
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
-  var numImages = 11;
+  var numImages = 9;
   addImages(numImages);
   var images = document.querySelectorAll(".stereograms .row a"); 
   images.forEach(function(image) {

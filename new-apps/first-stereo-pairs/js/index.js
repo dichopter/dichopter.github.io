5422002eb25dca93873861dc881dc44c0c482508
dichopter <dichopter@gmail.com>
2018-12-27 09:36:40 -0700

    added set cookie functionality to the First Stereo Pairs? page

--- new-apps/first-stereo-pairs/js/index.js
+++ new-apps/first-stereo-pairs/js/index.js
@@ -1,2 +1,23 @@
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+    
+    var images = document.querySelectorAll("img"); 
+    images.forEach(function(image) {
+      image.parentElement.addEventListener("click", function() {
+        setCookie("imageNum",image.getAttribute("imageNum"),1);
+      });
+    });
+    
+  });
+  
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  

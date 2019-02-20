36a322a3b68fb5018418deb0274bf289e1c1790b
root <root@DESKTOP-I1V810S.localdomain>
2019-02-20 08:37:59 -0500

    fixed alert

--- stereogram/js/index.js
+++ stereogram/js/index.js
@@ -183,7 +183,7 @@ function windowResized() {
 }
 
 window.addEventListener("resize", function() {
-  alert("window has been resized!!");
+  //alert("window has been resized!!");
   document.querySelector("canvas").height =  document.querySelector("body").offsetHeight;
   document.querySelector("canvas").width =  document.querySelector("body").offsetWidth;
 });

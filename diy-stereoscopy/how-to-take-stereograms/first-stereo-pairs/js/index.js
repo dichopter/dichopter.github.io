0e790c5f91c2f79a9224f9de1a3d675e60137e22
xavierrocks <xruizify@gmail.com>
2019-02-20 14:58:22 -0500

    added page for taking stereograms

---
+++ diy-stereoscopy/how-to-take-stereograms/first-stereo-pairs/js/index.js
@@ -0,0 +1,23 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+    
+    var images = document.querySelectorAll("img"); 
+    images.forEach(function(image) {
+      image.parentElement.addEventListener("click", function() {
+        setCookie("imageNum",image.parentElement.getAttribute("imageNum"),1);
+      });
+    });
+    
+  });
+  
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  

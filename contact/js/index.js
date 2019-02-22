331591421a7fdf4a4a0e572d670b37d267e45b06
xavierrocks <xruizify@gmail.com>
2019-02-22 16:04:23 -0500

    added profile images

---
+++ contact/js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var numImages = 18;
+    addImages(numImages);
+    var images = document.querySelectorAll("a .profile-image");
+    images.forEach(function (image) {
+        image.addEventListener("click", function () {
+            setCookie("imageNum", image.getAttribute("imageNum"), 1);
+        });
+    });
+
+});
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}

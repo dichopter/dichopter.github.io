1c570dd89c9719e81ba6df3dec7bffef3255204e
xavierrocks <xruizify@gmail.com>
2019-04-02 11:20:16 -0400

    new logo sizing, modal opens right away

--- diy-stereoscopy/upload-your-own-stereo-pairs/js/index.js
+++ diy-stereoscopy/upload-your-own-stereo-pairs/js/index.js
@@ -35,8 +35,10 @@ function setup() {
   resetButton.mousePressed(resetImages);
   console.log("...");
   info = createElement("div", "<div id='modal1' class='modal'><div class='modal-content'><h4>Modal Header</h4><p>A bunch of text</p></div><div class='modal-footer'><a href='#!' class='modal-close waves-effect waves-green btn-flat'>Agree</a></div></div>");
-  var elems = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elems, {});
+  var elems = document.querySelector('.modal');
+  var instance = M.Modal.init(elems, {});
+  instance.open();
+  
 }
 
 function draw() {

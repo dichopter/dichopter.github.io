04ac9d53aace754d5ada7acef93f6fc9ca8083d5
xavierrocks <xruizify@gmail.com>
2019-04-19 13:16:26 -0400

    added requested pairs... must still fix image resizing issue

--- stereograms/js/index.js
+++ stereograms/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var numImages = 16;
+  var numImages = 19;
   addImages(numImages);
   var images = document.querySelectorAll(".stereograms .row a"); 
   images.forEach(function(image) {

9e0409f63929bb666d42ce1fce2d0c893a555fce
Xavier Ruiz <xruizify@gmail.com>
2019-05-01 14:01:58 -0400

    added pixel density

--- stereogram/js/index.js
+++ stereogram/js/index.js
@@ -38,6 +38,7 @@ function preload(){
 }
 
 function setup() {
+  pixelDensity(1);
   stereoImage.loadPixels(); // hopefully makes the preload function blocking
   c = createCanvas(window.innerWidth, window.innerHeight);
   background(0);
